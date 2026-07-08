00ca5e4ee8b4db210efb8558906d3f90b16d3391
Shubham Nagpal <shubhamnagpal018@gmail.com>
2026-07-08 23:25:15 +0530

    completed the configuration

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,22 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUIDNARY_CLOUD_NAME, 
+  api_key: process.env.CLOUIDNARY_API_KEY, 
+  api_secret: process.env.CLOUIDNARY_API_SECRET
+});
+
+const uploadOnClouidanry = async (localfilePath) =>{
+    try {
+        if (!localfilePath) return null
+        //upload the file on cloudinary 
+ 
+         cloudinary.uploader.upload(localfilePath,{
+            resource_type: "auto"
+         })
+
+    } catch (error) {
+        
+    }
+}
